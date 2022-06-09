2d4b9ab8070c848716407c896ac9be3e4836c729
Ángel Sánchez <mektigboy@protonmail.com>
2022-06-08 22:04:31 -0500

    Create & update Coin.sol

---
+++ Coin.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.14;
+
+contract Coin {
+    // The keyword "public" makes variables accesible from other contracts.
+    address public minter;
+    mapping (address => uint256) public balances;
+
+    // Events allow clients to react to specific contract changes you declare.
+    event Sent(address from, address to, uint256 amount);
+
+    // Constructor code is only run when the contract is created.
+    constructor() {
+        minter = msg.sender;
+    }
+
+    // Sends an amount of newly created coins to an address.
+    // Can only be called by the contract creator.
+    function mint(address receiver, uint256 amount) public {
+        require(msg.sender == minter);
+        balances[receiver] += amount;
+    }
+}
